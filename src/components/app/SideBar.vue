a6f80c1477b1fcf3d2cd7aa10c30b0b27934ec2b
Staburov Alexey <gilbertfrost@yandex.ru>
2024-01-10 16:25:46 +0300

    side bar proces

---
+++ src/components/app/SideBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <router-link
+      active-class="active"
+      to="/"
+      exact
+      v-for="link in links"
+      :key="link.url"
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{ link.title }}</a>
+    </router-link>
+  </ul>
+</template>
+<script>
+export default {
+  data: () => ({
+    links: [
+      { title: "Счет", url: "/" },
+      { title: "История", url: "/history" },
+      { title: "Планирование", url: "/planning" },
+      { title: "Новая запись", url: "/record" },
+      { title: "Категория", url: "/categories" },
+    ],
+  }),
+};
+</script>

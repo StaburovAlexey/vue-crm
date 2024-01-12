bf146a6ff06388d42a29783cae9f8a55f062040d
Staburov Alexey <gilbertfrost@yandex.ru>
2024-01-13 02:48:08 +0300

    validation form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,32 +1,33 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
         <input
-            id="email"
-            type="text"
-            class="validate"
-        >
+          id="email"
+          type="text"
+          v-model.trim="email"
+          :class="{
+            invalid: v$.email.$invalid,
+          }"
+        />
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small class="helper-text invalid" v-if="!v$.email.required"
+          >Поле Email не должно быть пустым
+        </small>
+        <!-- <small class="helper-text invalid" v-else-if="v$.email.email"
+          >Введите коректный Email
+        </small> -->
       </div>
       <div class="input-field">
-        <input
-            id="password"
-            type="password"
-            class="validate"
-        >
+        <input id="password" type="password" class="validate" />
         <label for="password">Пароль</label>
         <small class="helper-text invalid">Password</small>
       </div>
     </div>
     <div class="card-action">
       <div>
-        <button
-            class="btn waves-effect waves-light auth-submit"
-            type="submit"
-        >
+        <button class="btn waves-effect waves-light auth-submit" type="submit">
           Войти
           <i class="material-icons right">send</i>
         </button>
@@ -34,8 +35,38 @@
 
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/register">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
+<script>
+import { useVuelidate } from "@vuelidate/core";
+import { email, required, minLength } from "@vuelidate/validators";
+export default {
+  name: "loginForm",
+  setup() {
+    return { v$: useVuelidate() };
+  },
+  data: () => ({
+    email: "",
+    password: "",
+  }),
+  methods: {
+    submitHandler() {
+      if (this.v$.$invalid) {
+        this.v$.$touch();
+        return;
+      }
+
+      this.$router.push("/");
+    },
+  },
+  validations() {
+    return {
+      email: { email, required },
+      password: { required, minLength: minLength(6) },
+    };
+  },
+};
+</script>
